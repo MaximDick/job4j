c882715eb63c42dc186494b4c27b9ddee893d3c9
MaximDick <maxim1994barca@gmail.com>
2019-01-07 23:13:40 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+ * Class Calculate display a message : "Hello World" 
+ * @autor Maxim Dick (maxim1994barca@gmail.com)
+ * @since 07.01.2019
+*/
+
 public class Calculate{
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
 	public static void main (String[] args){
 		System.out.println("Hello World!");
 	}
