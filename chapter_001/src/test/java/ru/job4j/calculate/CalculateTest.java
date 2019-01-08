3b82ef1d11db4f2fd47cbcc4422f7b64977dd211
MaximDick <maxim1994barca@gmail.com>
2019-01-09 00:00:57 +0300

    Test

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -5,23 +5,23 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Maxim Dick (maxim1994barca@gmail.com)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Maxim Dick (maxim1994barca@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-	/**
-* Test echo.
-*/
-@Test
-public void whenTakeNameThenThreeEchoPlusName() {
-    String input = "Maxim Dick";
-    String expect = "Echo, echo, echo : Maxim Dick"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenThreeEchoPlusName() {
+        String input = "Maxim Dick";
+        String expect = "Echo, echo, echo : Maxim Dick";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
 
 }
