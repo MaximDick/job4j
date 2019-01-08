f42f6a9b721b7856aed2033bdffc7b788a22b25b
MaximDick <maxim1994barca@gmail.com>
2019-01-08 19:23:39 +0300

    1.3.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -3,7 +3,7 @@ package ru.job4j.calculate;
 /**
  * Class Calculate display a message : "Hello World" 
  * @author Maxim Dick (maxim1994barca@gmail.com)
- * @since 07.01.2019.
+ * @since 07.01.2019
 */
 
 public class Calculate{
