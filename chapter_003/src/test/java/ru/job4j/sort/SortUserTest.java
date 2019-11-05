e0f09aa93cf9fd341b1d7983bc5c3bbd56c671c0
MaximDick <maxim1994barca@gmail.com>
2019-11-06 00:07:05 +0300

    0. Stream API улучшения [#58167]

--- chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -13,41 +13,65 @@ import static org.junit.Assert.*;
  */
 
 public class SortUserTest {
+
     @Test
     public void whenSortedUserByAge() {
         SortUser sortUser = new SortUser();
-        List<User> list = new ArrayList<>();
-        list.add(new User("Messi", 31));
-        list.add(new User("Suarez", 32));
-        list.add(new User("Neymar", 27));
-        list.add(new User("Xavi", 39));
-        list.add(new User("Iniesta", 35));
+        List<User> list = List.of(
+                new User("Messi", 31),
+                new User("Suarez", 32),
+                new User("Neymar", 27),
+                new User("Xavi", 39),
+                new User("Iniesta", 35));
+
         Set<User> result = sortUser.sort(list);
         assertThat(result.iterator().next().getAge(), is(27));
     }
 
     @Test
     public void whenSortedUserByName() {
+//        SortUser sortUser = new SortUser();
+//        List<User> list = new ArrayList<>();
+//        list.add(new User("Messi", 31));
+//        list.add(new User("Suarez", 32));
+//        list.add(new User("Neymar", 27));
+//        list.add(new User("Xavi", 39));
+//        list.add(new User("Iniesta", 35));
+//        List<User> result = sortUser.sortNameLength(list);
+//        assertThat(result.toString(), is("[Xavi 39, Messi 31, Suarez 32, Neymar 27, Iniesta 35]"));
+
+        List<User> users = List.of(
+        new User("Messi", 31),
+        new User("Suarez", 32),
+        new User("Ronaldinho", 27));
         SortUser sortUser = new SortUser();
-        List<User> list = new ArrayList<>();
-        list.add(new User("Messi", 31));
-        list.add(new User("Suarez", 32));
-        list.add(new User("Neymar", 27));
-        list.add(new User("Xavi", 39));
-        list.add(new User("Iniesta", 35));
-        List<User> result = sortUser.sortNameLength(list);
-        assertThat(result.toString(), is("[Xavi 39, Messi 31, Suarez 32, Neymar 27, Iniesta 35]"));
+        List<User> result = sortUser.sortNameLength(users);
+        List<User> check = List.of(
+                new User("Messi", 31),
+                new User("Suarez", 32),
+                new User("Ronaldinho", 27)
+        );
+        assertThat(result, is(check));
     }
 
     @Test
     public void whenSortedUserByFields() {
+//        SortUser sortUser = new SortUser();
+//        List<User> list = new ArrayList<>();
+//        list.add(new User("Messi", 31));
+//        list.add(new User("Messi", 19));
+//        list.add(new User("Neymar", 27));
+//        list.add(new User("Valdes", 39));
+//        list.add(new User("Henry", 35));
+
         SortUser sortUser = new SortUser();
-        List<User> list = new ArrayList<>();
-        list.add(new User("Messi", 31));
-        list.add(new User("Messi", 19));
-        list.add(new User("Neymar", 27));
-        list.add(new User("Valdes", 39));
-        list.add(new User("Henry", 35));
+        List<User> list = List.of(
+                new User("Messi", 31),
+                new User("Suarez", 32),
+                new User("Neymar", 27),
+                new User("Xavi", 39),
+                new User("Iniesta", 35));
+
         List<User> result = sortUser.sortByAllFields(list);
         assertThat(result.toString(), is("[Henry 35, Messi 19, Messi 31, Neymar 27, Valdes 39]"));
     }
