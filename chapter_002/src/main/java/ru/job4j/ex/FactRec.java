4c05e0fb0320e2d0e7fda06f0ecdf3b37a40f442
Maxim Dick <maxim1994barca@gmail.com>
2020-01-27 17:47:23 +0300

    0.6. Error - исключения связанные с работой виртуальной машины

--- chapter_002/src/main/java/ru/job4j/ex/FactRec.java
+++ chapter_002/src/main/java/ru/job4j/ex/FactRec.java
@@ -4,9 +4,11 @@ public class FactRec {
     public static int calc(int n) {
         if (n == 1) {
             return 1;
-        } else return (n - 1) * n;
-        //return n == 1 ? 1 : (n -1) * n; можно через тернарный оператор.
+        } else {
+            return (n - 1) * n;
+        }
     }
+        //return n == 1 ? 1 : (n -1) * n; можно через тернарный оператор.
 
     public static void main(String[] args) {
         int rsl = calc(6);
