77905c1560ee994f1d0696d0abad86017efee9a4
Maxim Dick <maxim1994barca@gmail.com>
2020-02-21 16:08:49 +0300

    Крестики-нолики на JavaFX

--- tictactoe/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ tictactoe/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -1,5 +1,6 @@
 package ru.job4j.tictactoe;
 
+import java.util.Arrays;
 import java.util.function.Predicate;
 
 public class Logic3T {
@@ -24,32 +25,45 @@ public class Logic3T {
     }
     /**проверяет есть ли в поле выигрышные комбинации для Крестика.*/
     public boolean isWinnerX() {
-        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0)
+        return /*this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0)
                 || this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0)
                 || this.fillBy(Figure3T::hasMarkX, 0, 2, 1, 0)
                 || this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1)
                 || this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1)
                 || this.fillBy(Figure3T::hasMarkX, 2, 0, 0, 1)
                 || this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)
-                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1);
+                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1);*/
+        isWin(Figure3T::hasMarkX);
+
     }
 
     /**проверяет есть ли в поле выигрышные комбинации для Нолика.*/
     public boolean isWinnerO() {
-        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0)
+        return /*this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0)
                 || this.fillBy(Figure3T::hasMarkO, 0, 1, 1, 0)
                 || this.fillBy(Figure3T::hasMarkO, 0, 2, 1, 0)
                 || this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1)
                 || this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1)
                 || this.fillBy(Figure3T::hasMarkO, 2, 0, 0, 1)
                 || this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1)
-                || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1);
+                || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1);*/
+        isWin(Figure3T::hasMarkO);
     }
 
+    public boolean isWin(Predicate<Figure3T> pr) {
+        return this.fillBy(pr, 0, 0, 1, 0)
+                || this.fillBy(pr, 0, 0, 0, 1)
+                || this.fillBy(pr, 0, 0, 1, 1)
+                || this.fillBy(pr, this.table.length - 1, 0, -1, 1)
+                || this.fillBy(pr, 0, 2, 1, 0)
+                || this.fillBy(pr, 0, 1, 1, 0)
+                || this.fillBy(pr, 1, 0, 0, 1)
+                || this.fillBy(pr, 2, 0, 0, 1);
+    }
     /**
      * проверяет, если ли пустые клетки для новых ходов.*/
     public boolean hasGap() {
-        boolean result = false;
+        /*boolean result = false;
         for (int indexOut = 0; indexOut < this.table.length; indexOut++) {
             for (int indexIn = 0; indexIn < this.table.length; indexIn++) {
                 Figure3T cell = this.table[indexOut][indexIn];
@@ -58,6 +72,10 @@ public class Logic3T {
                 }
             }
         }
-        return result;
+        return result;*/
+
+        return Arrays.stream(table)
+                .flatMap(Arrays::stream)
+                .anyMatch(figure -> !figure.hasMarkX() && !figure.hasMarkO());
     }
 }
