e08e3132a37a38954743849952d6cf1e11af27f9
Maxim Dick <maxim1994barca@gmail.com>
2020-02-26 12:13:52 +0300

    Идентичные тексты. [#212655]

--- chapter_003/src/main/java/ru/job4j/collection/set/FullSearch.java
+++ chapter_003/src/main/java/ru/job4j/collection/set/FullSearch.java
@@ -3,6 +3,11 @@ package ru.job4j.collection.set;
 import java.util.HashSet;
 import java.util.List;
 
+/**
+ * @author Maxim Dick (maxim1994barca@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class FullSearch {
     public static HashSet<String> extractNumber(List<TaskSet> list) {
         HashSet<String> numbers = new HashSet<>();
--- chapter_003/src/main/java/ru/job4j/collection/set/TaskSet.java
+++ chapter_003/src/main/java/ru/job4j/collection/set/TaskSet.java
@@ -1,5 +1,10 @@
 package ru.job4j.collection.set;
 
+/**
+ * @author Maxim Dick (maxim1994barca@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class TaskSet {
     private String number;
     private String description;
